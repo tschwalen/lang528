9f9f37fc08d2c7f204a75fc2d83100eea86a5795
Thomas Schwalen <thomasschwalen@gmail.com>
2024-04-14 16:05:47 -0500

    progress

---
+++ scripts/run_e2e_tests.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+from util import get_project_root_directory
+
+import os
+import subprocess
+
+EXECUTABLE_NAME = "output"
+EXECUTABLE_PATH = None
+
+def run_e2e_test(directory: str, file: str):
+    print(f"Running {file}")
+    res = subprocess.run(
+        [EXECUTABLE_PATH, "--exec", f"--input={directory}/{file}"],
+        stdout=subprocess.PIPE, 
+        stderr=subprocess.PIPE
+    )
+
+    stdout = res.stdout.decode("utf-8")
+    stderr = res.stderr.decode("utf-8")
+    retcode = res.returncode
+
+    for output_line in stdout.split("\n"):
+        if output_line.startswith("#"):
+            parts = output_line.split("#")
+            macro = parts[1] # pass, fail, log, etc.
+            msg = parts[2].strip()
+
+            if macro == "PASS":
+                print("\033[32m - Passed check" + f": \"{msg}\"" if msg != "" else "" + "\033[0m")
+
+    
+
+
+
+
+def main():
+    global EXECUTABLE_PATH
+
+    root_dir = get_project_root_directory()
+    EXECUTABLE_PATH = f"{root_dir}/{EXECUTABLE_NAME}"
+    e2e_test_dir = f"{root_dir}/examples/e2e"
+
+    for e2e_test in os.listdir(e2e_test_dir):
+        run_e2e_test(e2e_test_dir, e2e_test)
+    
+
+if __name__ == "__main__":
+    main()

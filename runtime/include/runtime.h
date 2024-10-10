a596b46e2826b5e69aa0f9ddbf4964ce17ec8a78
Thomas Schwalen <thomasschwalen@gmail.com>
2024-10-10 00:09:55 -0500

    runtime progress

--- runtime/include/runtime.h
+++ runtime/include/runtime.h
@@ -1,3 +1,7 @@
 #pragma once
 
+#include "datatype.h"
+
 int placeholder(int x);
+
+void builtin_print(RuntimeObject* arg);
--- runtime/src/runtime.c
+++ runtime/src/runtime.c
@@ -1,5 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "runtime.h"
+#include "datatype.h"
 
 int placeholder(int x) {
     return x + x;
+}
+
+void _print_vector(Vector* vec);
+
+void _print_helper(RuntimeObject* obj) {
+    // prints object with no newline
+    switch (obj->type) {
+        case T_NOTHING:
+            printf("nothing");
+            return;
+        case T_BOOL:
+            printf(obj->value.v_bool ? "true" : "false");
+            return;
+        case T_INT:
+            printf("%lld", obj->value.v_int);
+            return;
+        case T_FLOAT:
+            printf("%f", obj->value.v_float);
+            return;
+        case T_STRING:
+            printf("%s", obj->value.v_str->contents);
+            return;
+        case T_VECTOR:
+            _print_vector(obj->value.v_vec);
+            return;
+        case T_DICT:
+        case T_MODULE:
+        case T_FUNCTION:
+        default:
+            break;
+    }
+    printf("NOT IMPLEMENTED");
+    exit(1);
+}
+
+void _print_vector(Vector* vec) {
+    printf("[");
+    // fencepost problem
+    int i = 0;
+    for(; i < vec->size - 1; ++i) {
+        _print_helper(&vec->contents[i]);
+        printf(", ");
+    }
+    _print_helper(&vec->contents[i]);
+    printf("]");
+}
+
+void builtin_print(RuntimeObject* arg) {
+    _print_helper(arg);
+    printf("\n");
 }

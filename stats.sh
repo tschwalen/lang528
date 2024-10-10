60ab5f25e15de47c2645ddd3c9e96a98b9432dcd
Thomas Schwalen <thomasschwalen@gmail.com>
2024-10-09 23:45:33 -0500

    count C loc

--- stats.sh
+++ stats.sh
@@ -6,3 +6,6 @@ find . -type f \( -name "*.cpp" -o -name "*.h" \) -maxdepth 2 -exec wc -l {} +
 # The same, but for python
 printf "\nPython LoC:\n" 
 find . -type f -name "*.py" -exec wc -l {} +
+
+printf "\nC LoC:\n" 
+find ./runtime -type f \( -name "*.c" -o -name "*.h" \) -maxdepth 2 -exec wc -l {} + 

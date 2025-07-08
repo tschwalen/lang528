bfcc14704b1ecc59a4aa5e91033eb5fdb1ab0258
Thomas Schwalen <thomasschwalen@gmail.com>
2025-07-07 23:19:48 -0500

    add shell to stats.sh LoC count script

--- stats.sh
+++ stats.sh
@@ -7,5 +7,9 @@ find . -type f \( -name "*.cpp" -o -name "*.h" \) -maxdepth 2 -exec wc -l {} +
 printf "\nPython LoC:\n" 
 find . -type f -name "*.py" -exec wc -l {} +
 
+# The same, but for python
+printf "\nShell LoC:\n" 
+find . -type f -name "*.sh" -exec wc -l {} +
+
 printf "\nC LoC:\n" 
 find ./runtime -type f \( -name "*.c" -o -name "*.h" \) -maxdepth 2 -exec wc -l {} + 
